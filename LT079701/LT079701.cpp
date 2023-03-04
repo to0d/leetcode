997c59c953e6ccd85b6f5272943329732858ce1b
lylybj <lylybj@cn.ibm.com>
2023-03-04 15:07:55 +0800

    refine

--- LT079701/LT079701.cpp
+++ LT079701/LT079701.cpp
@@ -3,63 +3,76 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> rst;
-        vector<int> path, indexs;
-        unordered_set<int> visited;
-        int target = graph.size() - 1;
-        
-        path.push_back(0);
-        visited.insert(0);
+        m_rst.clear();
+        m_target = graph.size()-1;
+        deepVisitGraph(graph, 0);
+        return m_rst;
+    }
+
+    void visit(vector<vector<int>>& graph, vector<int>& path) {
+        int cur_node = path.back();
+        if( cur_node == m_target)
+            m_rst.push_back(path);
+    }
+
+    void deepVisitGraph(vector<vector<int>>& graph, int start) {
+        vector<int> path, visited_indexs, visited_nodes(graph.size(), 0);
+        path.push_back(start);
+        visited_nodes[start] = 1; 
         bool push = true;
                  
         while( !path.empty() )
-        {   int cur = path.back();
+        {   int cur_node = path.back();
             if( push )
-            {   if( cur == target)
-                    rst.push_back(path);
+            {   
+                visit(graph, path);
 
-                vector<int>& links = graph[cur];
+                // find next unvisited node
+                vector<int>& links = graph[cur_node];
                 int find = -1, len  = links.size();
                 for(int i = 0; find == -1 && i < len; ++i)
                 {   int next = links[i];
-                    if( visited.count(next) == 0 )
+                    if( visited_nodes[next] == 0 )
                         find = i;
                 }
 
                 if( find != -1 )
                 {   path.push_back(links[find]);
-                    indexs.push_back(find);
-                    visited.insert(links[find]);
+                    visited_indexs.push_back(find);
+                    visited_nodes[links[find]] = 1;
                 }
                 else
                     push = false;
             }
             else
-            {   if( cur == 0 )
+            {   if( cur_node == 0 )
                     break;
                 
-                visited.erase(cur);
+                visited_nodes[cur_node] = 0;
                 path.pop_back();
-                cur = path.back();
-                vector<int>& links = graph[cur];
-                int len = links.size(), idx = indexs.back(), find = -1;
-                indexs.pop_back();
+                
+                cur_node = path.back();
+                vector<int>& links = graph[cur_node];
+                int len = links.size(), idx = visited_indexs.back(), find = -1;
+                visited_indexs.pop_back();
                 for(int i = idx + 1; find == -1 && i < len; ++i)
                 {   int next = links[i];
-                    if( visited.count(next) == 0 )
+                    if( visited_nodes[next] == 0 )
                         find = i;
                 }
 
                 if( find != -1 )
                 {   path.push_back(links[find]);
-                    indexs.push_back(find);
-                    visited.insert(links[find]);
+                    visited_indexs.push_back(find);
+                    visited_nodes[links[find]] = 1;
                     push = true;
                 }
             }
         }
-        return rst;
     }
+    
+    vector<vector<int>> m_rst;
+    int m_target;
 };
 
 
@@ -83,6 +96,6 @@ int main(void)
 //
 // 2023-01-18: Runtime 23ms 38.33% Memory 11.7MB 60.97%, https://leetcode.com/problems/all-paths-from-source-to-target/submissions/880452388/
 // 2023-02-25: Runtime 15ms 59.40% Memory 11.8MB 60.83%, https://leetcode.com/problems/all-paths-from-source-to-target/submissions/904431807/
-
+// 2023-03-04: Runtime 13ms 78.97% Memory 11.9MB 60.26%, https://leetcode.com/problems/all-paths-from-source-to-target/submissions/908660294/
 
 
