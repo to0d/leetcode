32ae3f37359cf6db8c91ae1decaab20cef2691a9
lylybj <lylybj@cn.ibm.com>
2023-03-07 17:12:41 +0800

    refine and add tag "拓扑排序"

--- LT020701/LT020701.cpp
+++ LT020701/LT020701.cpp
@@ -1,38 +1,41 @@
 #include <lt_help/lt.h>
 
+int topologicalOrder(int total, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> nextIndexs(total);
+    vector<int> preCount(total, 0);
+    for( auto p : prerequisites)
+    {   nextIndexs[p[1]].push_back(p[0]);
+        preCount[p[0]]++;
+    }
+
+    queue<int> noPreQueue;   
+    for( int i = 0; i < total; ++i)
+        if( preCount[i] == 0 )
+            noPreQueue.push(i);
+
+    int count = 0;
+    while( !noPreQueue.empty() )
+    {   int pre = noPreQueue.front(); noPreQueue.pop();
+        ++count;
+        for( int next : nextIndexs[pre] )
+            if( --preCount[next] == 0 )
+                noPreQueue.push( next );
+    }
+
+    return count;
+}
+
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> nextMap;
-        unordered_map<int, int > preMap;
-        queue<int> noPreQueue;
-        int visitedCourse = 0 ;
-
-        for( auto p : prerequisites)
-        {   nextMap[p[0]].push_back(p[1]);
-            preMap[p[1]]++;
-        }
-        
-        for( int i = 0; i < numCourses; ++i)
-            if( preMap[i] == 0 )
-                noPreQueue.push(i);
-        
-        while( !noPreQueue.empty() )
-        {   int course = noPreQueue.front();
-            noPreQueue.pop();
-            ++visitedCourse;
-            for( int next : nextMap[course] )
-                if( --preMap[next] == 0 )
-                    noPreQueue.push( next);
-        }
-
-        return visitedCourse == numCourses;
+        int count = topologicalOrder(numCourses, prerequisites);   
+        return count == numCourses;
     }
 };
-    
+
 
    
-void test(int numCourses, vector<vector<int>>& prerequisites)
+void test(int numCourses, vector<vector<int>> prerequisites)
 {
     cout << "input: num=" << numCourses << ", pre=";
     outputVector(prerequisites);
@@ -42,13 +45,12 @@ void test(int numCourses, vector<vector<int>>& prerequisites)
 
 int main(void)
 {
-    vector<vector<int>> prerequisites = {{1,0}};
-    test(2, prerequisites);
+    test(2, {{1,0}});
 }
 
 // Result 
 //
 // 2022-11-23: Runtime 76ms 6.4% Memory 15.1MB 18.4%, https://leetcode.com/problems/course-schedule/submissions/848547625/
 // 2023-02-22: Runtime 51ms 11.28% Memory 15.2MB 17.79%, https://leetcode.com/problems/course-schedule/submissions/902630983/
-
+// 2023-03-07: Runtime 20ms 84.83% Memory 14.1MB 43.84%, https://leetcode.com/problems/course-schedule/submissions/910710403/
 
--- LT021001/LT021001.cpp
+++ LT021001/LT021001.cpp
@@ -1,40 +1,37 @@
 #include <lt_help/lt.h>
 
+int topologicalOrder(int total, vector<vector<int>>& prerequisites, vector<int>& orders) {
+    vector<vector<int>> nextIndexs(total);
+    vector<int> preCount(total, 0);
+    for( auto p : prerequisites)
+    {   nextIndexs[p[1]].push_back(p[0]);
+        preCount[p[0]]++;
+    }
+
+    queue<int> noPreQueue;   
+    for( int i = 0; i < total; ++i)
+        if( preCount[i] == 0 )
+            noPreQueue.push(i);
+
+    int count = 0;
+    while( !noPreQueue.empty() )
+    {   int pre = noPreQueue.front(); noPreQueue.pop();
+        orders.push_back(pre);
+        ++count;
+        for( int next : nextIndexs[pre] )
+            if( --preCount[next] == 0 )
+                noPreQueue.push( next );
+    }
+
+    return count;
+}
+
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> nextCourseMap;
-        queue<int> noPreQueue;   
-        vector<int> preCount(numCourses,0);
-        vector<int> res;
-
-        for( auto pa : prerequisites)
-        {   int to = pa[0], from = pa[1];
-            if( nextCourseMap.count(from) == 0)
-                nextCourseMap[from] = vector<int>();
-                nextCourseMap[from].push_back(to);
-            preCount[to]++;
-        }
-        
-        for( int c = 0; c < numCourses; ++c)
-            if( preCount[c] == 0)
-                noPreQueue.push(c);
-        
-        while( !noPreQueue.empty() )
-        {   int c = noPreQueue.front();
-            noPreQueue.pop();               
-            res.push_back(c);
-            
-            if( nextCourseMap.count(c))
-                for( int to : nextCourseMap[c])
-                    if( --preCount[to] == 0 )
-                    noPreQueue.push(to);
-        }
-
-        if( res.size() != numCourses)
-            res.clear();
-
-        return res;
+        vector<int> courses;
+        int count = topologicalOrder(numCourses, prerequisites, courses);        
+        return count == numCourses ? courses : vector<int>();
     }
 };
 
@@ -52,8 +49,7 @@ void test(int numCourses, vector<vector<int>>& prerequisite)
 
 int main(void)
 {
-    vector<vector<int>> prerequisite = {{1,0},{2,0},{3,1},{3,2}};
-    test(4, prerequisite); 
+    test(4, {{1,0},{2,0},{3,1},{3,2}}); 
 }
 
 
@@ -61,5 +57,6 @@ int main(void)
 //
 // 2022-11-24: Runtime 61ms 7.48% Memory 14.6MB 26.5%, https://leetcode.com/problems/course-schedule-ii/submissions/849024852/
 // 2023-02-22: Runtime 26ms 52.91% Memory 14.7MB 24.24%, https://leetcode.com/problems/course-schedule-ii/submissions/902634171/
+// 2023-03-07: Runtime 25ms 56.16% Memory 14.2MB 34.95%, https://leetcode.com/problems/course-schedule-ii/submissions/910708858/
 
 
