a4e1ce1b4c210109873e854ee25b938c5a76c45a
lylybj <lylybj@cn.ibm.com>
2023-05-26 22:17:38 +0800

    up

--- LT015201/LT015201.cpp
+++ LT015201/LT015201.cpp
@@ -4,35 +4,25 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int size = nums.size(), e0 = nums[0], rst = e0;
-        vector<int> dp_p(size+1, 0), dp_n(size+1, 0);
-        dp_p[0] = e0 > 0 ? e0 : 0;
-        dp_n[0] = e0 < 0 ? e0 : 0;
+        int dp_p = e0 > 0 ? e0 : 0;
+        int dp_n = e0 < 0 ? e0 : 0;
         for(int i = 1; i < size; ++i)
         {   int num = nums[i], p = 0, n = 0;
             if( num > 0 )
-            {   p = num;
-                if( dp_p[i-1] > 0 )
-                    p = max(p, dp_p[i-1]*num );
-                if( dp_n[i-1] < 0 )
-                    n = min(p, dp_n[i-1]*num );
+            {   p = max(num, dp_p*num);
+                n = min(num, dp_n*num );
             }
             else if( num < 0 )
-            {   n = num;
-                if( dp_p[i-1] > 0 )
-                    n = min(p, dp_p[i-1]*num );
-                if( dp_n[i-1] < 0 )
-                    p = max(p, dp_n[i-1]*num );
+            {   p = max(num, dp_n*num );
+                n = min(num, dp_p*num );
             }
-            if( p > 0 )
-                dp_p[i] = p;
-            if( n < 0 )
-                dp_n[i] = n;
+            dp_p = p > 0 ? p : 0;
+            dp_n = n < 0 ? n : 0;
             rst = max(rst, p);
         }
         return rst;
     }
 };
-
 
 
 void test(vector<int> nums)
@@ -53,6 +43,7 @@ int main(void)
 //
 // 2023-01-03: Runtime 16ms 15.50% Memory 14.7MB 5.16%, https://leetcode.com/problems/maximum-product-subarray/submissions/870508258/
 // 2023-03-12: Runtime 8ms 57.41% Memory 14.1MB 8.37%, https://leetcode.com/problems/maximum-product-subarray/submissions/913876463/
+// 2023-05-26: Runtime 6ms 75.90% Memory 13.8MB 36.68%, https://leetcode.com/problems/maximum-product-subarray/submissions/957543868/
 
 
 
