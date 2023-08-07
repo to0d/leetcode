981b0a8ac466f7c40c14cc7766e57e61b4e7fa51
todd <to0d@outlook.com>
2023-08-07 20:41:26 +0800

    up

--- update.cmd
+++ update.cmd
@@ -1,4 +1,4 @@
 @echo off  
 C:
 cd C:\data\note\inote
-call bin\alpha_note_util.cmd main.DoUpdateLeetcodeSummary "C:\data\public\leetcode" "C:\data\note\note_public\note\Algorithm\notes\Note Reference - Algorithm\Leetcode Summary.md" "--conf=C:\data\note\inote\conf"
+call bin\alpha_note_util.cmd main.DoUpdateLeetcodeSummary "C:\data\public\leetcode" "C:\data\note\note_public\note\Leetcode\notes\Leetcode Summary\Leetcode Summary2.md" "--conf=C:\data\note\inote\conf"
